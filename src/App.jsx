1f73cda3a7aed1c9dfbcdf43a0035e74a9bdc055
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-31 11:06:24 +0500

    Import Axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,15 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const App = () => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      const response = await axios.get(
-        "https://picsum.photos/v2/list?page=2&limit=100"
-      );
-      setImages(response.data);
-    };
-    fetchImages();
-  }, []);
-
+  const getDate = () => {
+    axios.get("https://picsum.photos/v2/list?page=2&limit=100");
+  };
   return (
     <div className="bg-black text-white h-screen ">
       <h1>Hello World!</h1>
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <img key={image.id} src={image.url} alt={image.title} />
-        ))}
-      </div>
+      <button className="bg-blue-500 p-2 rounded p-2 text-white font-bold" onClick={getDate}>
+        Get Date
+      </button>
     </div>
   );
 };
