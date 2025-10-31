d6e0faead39e0f3a9cea5f14be43ac85b6010a76
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-31 10:57:21 +0500

    Install Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return <div className="bg-black text-white h-screen ">
-    <h1>Hello World!</h1>
-  </div>;
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const response = await axios.get(
+        "https://picsum.photos/v2/list?page=2&limit=100"
+      );
+      setImages(response.data);
+    };
+    fetchImages();
+  }, []);
+
+  return (
+    <div className="bg-black text-white h-screen ">
+      <h1>Hello World!</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <img key={image.id} src={image.url} alt={image.title} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
